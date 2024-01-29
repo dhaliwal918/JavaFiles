d19bf5370b189434f684e3765ef5a1f39d14fb16
dhaliwal918 <dhaliwal090108@gmail.com>
2024-01-29 11:05:03 +0530

    Improve Quality of code

--- PrimeOrNot.java
+++ PrimeOrNot.java
@@ -1,27 +1,21 @@
 import java.util.*;
 public class PrimeOrNot {
-    public static void primeornot(float n){
-        int number = 2;
-        for(;;){
-            if(n==2){
-                System.out.println("The number is Prime");
-                return;
-            }
-            if(n%number==0){
-                System.out.println("The number is not prime");
-                return;
-            }
-            number++;
-            if(number==n){
-                break;
-            }
-        }
-        System.out.println("The number is prime");
-    }
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number : ");
-        float n = sc.nextFloat();
-        primeornot(n);
+        int n = sc.nextInt();
+        boolean prOrNot = primeornot(n);
+        System.out.println(prOrNot);
+    }
+    public static boolean primeornot(int n){
+        if(n==2){
+            return true;
+        }
+        for(int i = 2 ; i<n/2 ; i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
     }
 }
