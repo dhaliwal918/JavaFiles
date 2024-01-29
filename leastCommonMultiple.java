21e697df0187a8698c9af2bde39058645e370190
dhaliwal918 <dhaliwal090108@gmail.com>
2024-01-29 10:37:56 +0530

    Improve lcm code

--- leastCommonMultiple.java
+++ leastCommonMultiple.java
@@ -8,46 +8,14 @@ public class leastCommonMultiple {
         System.out.print("Enter the number : ");
         int num2 = sc.nextInt();
         int ans = LCM(num1, num2);
-        System.out.println("The LCM of " + num1 + " ans " + num2 + " is : " + ans);
+        System.out.println("The LCM of " + num1 + " and " + num2 + " is : " + ans);
     }
 
     public static int LCM(int num1, int num2) {
-        int ans = 1;
-        int var = num1+num2;
-        for (int i = 2; i <= var; i++) {
-            if (primeornot(i) == 1) {
-                System.out.println("The value of i = "+i);
-                while (num1%i==0||num2%i==0) {
-                    if(num1%i==0){
-                        ans = ans * i;
-                        num1 = num1/i;
-                        System.out.println(num1);
-                    }
-                    if(num2%i==0){
-                        ans = ans * i;
-                        num2 = num2/i;
-                        System.out.println(num2);
-                    }
-                }
+        for(int i = 1; ; i++){
+            if((i%num1==0)&&(i%num2==0)){
+                return i;
             }
         }
-        return ans;
-    }
-
-    public static int primeornot(float n) {
-        int number = 2;
-        for (;;) {
-            if (n == 2) {
-                return 1;
-            }
-            if (n % number == 0) {
-                return 0;
-            }
-            number++;
-            if (number == n) {
-                break;
-            }
-        }
-        return 1;
     }
 }
