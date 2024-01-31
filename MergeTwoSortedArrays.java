e1972cde9dbd7c878e34b92478e175aed6a8cb8c
dhaliwal918 <dhaliwal090108@gmail.com>
2024-01-31 09:56:39 +0530

    New file

---
+++ MergeTwoSortedArrays.java
@@ -0,0 +1,7 @@
+import java.util.*;
+public class MergeTwoSortedArrays{
+    public static void main(String[] args) {
+        int[]arr1 = {4,7,9,12,14,17,19,20,28,49};
+        int[]arr2 = {2,5,6,9,11,16,17,18,24,50};
+    }
+}
